0c5cccea6e5a49e071af56dae67db2d3e8533978
Daniel Parsley <daniel.parsley@hcahealthcare.com>
2022-08-17 13:22:24 -0500

    changed unzip location

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
 function installSFDX(){
   var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx/versions/7.160.0/65e206c/sfdx-v7.160.0-65e206c-linux-x64.tar.xz -q -P /tmp'
   var createDir = 'mkdir /tmp/sfdx'
-  var unzip = 'tar xJf /tmp/sfdx-linux-x64.tar.xz -C /tmp/sfdx --strip-components 1'
+  var unzip = 'tar xJf /tmp/sfdx-v7.160.0-65e206c-linux-x64.tar.xz -C /tmp/sfdx --strip-components 1'
   var install = 'echo "/tmp/sfdx/bin" >> $GITHUB_PATH'
   var version = '/tmp/sfdx/bin/sfdx --version && /tmp/sfdx/bin/sfdx plugins --core'
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+version, function(error, stdout, stderr){
